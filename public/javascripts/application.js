5babd76a031d332241f0292254650b1fc8c756c1
michaellennox <michaelctlennox@gmail.com>
2015-11-17 16:38:04 +0000

    Added JS to make styling buttons function

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -21,3 +21,27 @@ $(document).on('ready', function() {
 $(document).on('input', '#caption-text', function() {
   $("#caption").text($(this).val());
 })
+
+$(document).on('change', '#caption-left', function() {
+  $("#caption").css("left", $(this).val() + 'px')
+})
+
+$(document).on('change', '#caption-top', function() {
+  $("#caption").css("top", $(this).val() + 'px');
+});
+
+$(document).on('change', '#caption-width', function() {
+  $("#caption").css("width", $(this).val() + 'px');
+});
+
+$(document).on('change', '#caption-size', function() {
+  $("#caption").css("font-size", $(this).val() + 'px');
+});
+
+$(document).on('change', '#caption-colour', function() {
+  $("#caption").css("color", $(this).val());
+});
+
+$(document).on('change', '#caption-align', function() {
+  $("#caption").css("text-align", $(this).val());
+});
