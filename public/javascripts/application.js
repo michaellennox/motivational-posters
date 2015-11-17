08a52828de2bcdc2eae46e0143431a9009b66694
michaellennox <michaelctlennox@gmail.com>
2015-11-17 14:48:08 +0000

    Wrote JS for selecting an image

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,3 +6,10 @@ $(document).on('submit', '#search-form', function(e) {
   };
   findImagesOnGoogle(options);
 });
+
+$(document).on('click', '#search-results img', function() {
+  var url = $(this).data('url');
+  $("#workspace img").remove();
+  var img = $("<img>").attr('src', url);
+  $('#workspace').append(img);
+})
